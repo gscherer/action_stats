e9c29b6c3e76d84bea97a018e401c42372315ac4
Grayson Scherer <gscherer@users.noreply.github.com>
2021-01-06 06:43:33 -0700

    A small library for demonstrating a concurrency safe function

---
+++ action_stats/action_stats.go
@@ -0,0 +1,75 @@
+package action_stats
+
+import (
+    "sync"
+    "encoding/json"
+)
+
+type actionTime struct {
+    Action string   `json:"action"`
+    Time   int      `json:"time,omitempty"`
+    Avg    int      `json:"avg,omitempty"`
+    Count  int      `json:"count,omitempty"`
+}
+
+type actionStat struct {
+    count int
+    totalTime int
+}
+
+func (s *actionStat) avg() int {
+    return s.totalTime / s.count
+}
+
+func (s *actionStat) addTime(time int) {
+    s.count++
+    s.totalTime += time
+}
+
+type actionMap struct {
+    sync.RWMutex
+    store map[string]*actionStat
+}
+
+func (a *actionMap) addActionTime(atm actionTime) {
+    _, ok := a.store[atm.Action]
+    if !ok {
+        a.store[atm.Action] = &actionStat{count: 1, totalTime: atm.Time}
+    } else {
+        a.store[atm.Action].addTime(atm.Time)
+    }
+}
+
+func (a *actionMap) MarshalJSON() ([]byte, error) {
+    a.RLock()
+    defer a.RUnlock()
+    res := make([]actionTime, len(a.store))
+    i := 0
+    for action, stat := range a.store {
+        res[i] = actionTime{Action: action, Avg: stat.avg()}
+        i++
+    }
+    return json.Marshal(res)
+}
+
+func (a *actionMap) AddAction(req string) error {
+    a.Lock()
+    defer a.Unlock()
+    var atm actionTime
+    if err := json.Unmarshal([]byte(req), &atm); err != nil {
+        return err
+    }
+    a.addActionTime(atm)
+    return nil
+}
+
+func (a *actionMap) GetStats() string {
+    res, _ := a.MarshalJSON()
+    return string(res)
+}
+
+func NewActionMap() *actionMap {
+    var a actionMap
+    a.store = make(map[string]*actionStat)
+    return &a
+}
---
+++ action_stats/action_stats_test.go
@@ -0,0 +1,43 @@
+package action_stats
+
+import (
+    "fmt"
+    "sync"
+    "encoding/json"
+    "testing"
+)
+
+func TestConcurrentActions(t *testing.T) {
+    var wg sync.WaitGroup
+    actions := NewActionMap()
+    for i := 0; i < 100; i++ {
+        t := 10 + i
+        wg.Add(3)
+        go func() {
+            defer wg.Done()
+            actions.AddAction(fmt.Sprintf(`{"action": "jump", "time": %d}`, t))
+            actions.GetStats()
+        }()
+        go func() {
+            defer wg.Done()
+            actions.AddAction(fmt.Sprintf(`{"action": "walk", "time": %d}`, t))
+            actions.GetStats()
+        }()
+        go func() {
+            defer wg.Done()
+            actions.AddAction(fmt.Sprintf(`{"action": "run", "time": %d}`, t))
+            actions.GetStats()
+        }()
+    }
+    wg.Wait()
+    stats := actions.GetStats()
+    var times [3]actionTime
+    if err := json.Unmarshal([]byte(stats), &times); err != nil {
+        t.Fatal(err)
+    }
+    for _, atm := range times {
+        if atm.Avg != 59 {
+            t.Errorf("Expected avg of 59, got %d for action `%s`", atm.Avg, atm.Action)
+        }
+    }
+}
